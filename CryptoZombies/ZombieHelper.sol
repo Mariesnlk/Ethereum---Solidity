993d0fb531a448834f3dc989cc12c0ec7ceefd1e
Mariia Synelnyk <44849051+Mariesnlk@users.noreply.github.com>
2022-01-18 16:14:31 +0200

    attack funct

--- CryptoZombies/ZombieHelper.sol
+++ CryptoZombies/ZombieHelper.sol
@@ -11,7 +11,7 @@ contract ZombieHelper is ZombieFeeding {
     }
 
     function withdraw() external onlyOwner {
-        address payable _owner = address(uint160(owner()));
+        address _owner = owner();
         _owner.transfer(address(this).balance);
     }
 
@@ -27,16 +27,16 @@ contract ZombieHelper is ZombieFeeding {
     function changeName(uint256 _zombieId, string calldata _newName)
         external
         aboveLevel(2, _zombieId)
+        ownerOf(_zombieId)
     {
-        require(msg.sender == zombieToOwner[_zombieId]);
         zombies[_zombieId].name = _newName;
     }
 
     function changeDna(uint256 _zombieId, uint256 _newDna)
         external
         aboveLevel(20, _zombieId)
+        ownerOf(_zombieId)
     {
-        require(msg.sender == zombieToOwner[_zombieId]);
         zombies[_zombieId].dna = _newDna;
     }
 
